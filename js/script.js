f0e30b9bdae817e5dff998fa12c9e80ecb55d6a4
AJ Sorbello <ajsorbello@gmail.com>
2023-10-26 12:37:32 -0700

    1.9 commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+
 function newItem() {
-    // 1. Adding a new item to the list of items 
+
+    //1. Adding a new item to the list:
+
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -7,40 +10,29 @@ function newItem() {
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        let list = $('#list');
-        list.append(li);
-        $('#input').val(""); // Reset input field
+        $('#list').append(li);
     }
 
-    // 2. Crossing out an item from the list of items:
+    //2. Crossing out an item from the list of items:
     function crossOut() {
-        li.toggleClass('strike');
+        li.toggleClass("strike");
     }
 
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
 
-    li.on('dblclick', crossOut);
+    //3(i). Adding the delete button "X": 
 
-    // 3(i). Adding the delete Button "X":
     let crossOutButton = $('<crossOutButton></crossOutButton>');
     crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
-    crossOutButton.on('click', deleteListItem);
-
-    // 3(ii). Adding class delete (display: none) from the css:
+    crossOutButton.on("click", deleteListItem);
     function deleteListItem() {
-        li.addClass('delete');
+        li.addClass("delete")
     }
 
-    // 4. Recording the items:
+    //4. Reordering the items:
     $('#list').sortable();
-}
-
-// on Enter
-function newItemOnEnter(event) {
-    var x = event.code;
-    if (x == "Enter") {
-        event.preventDefault(); // Don't submit form
-        newItem();
-    }
 }
